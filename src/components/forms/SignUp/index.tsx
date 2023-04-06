678a81e8710680204effd424fc6eb0d9e538a72d
Karletty Elias <karletty.carolina@gmail.com>
2023-04-05 23:18:17 -0600

    añadiendo react-hook-forms para los formularios y añadiendo los servicios

--- src/components/forms/SignUp/index.tsx
+++ src/components/forms/SignUp/index.tsx
@@ -1,50 +1,55 @@
 import { Form, Button } from 'react-bootstrap';
 import '../styles.scss';
-import { SignUpUser } from '../../../types';
+import { SignUpForm } from '../../../types';
 import { useForm } from 'react-hook-form';
+import { servicesUser } from '../../../services/users';
 
 const SignUp = () => {
-      const { register, handleSubmit } = useForm<SignUpUser>();
+      const { register, handleSubmit } = useForm<SignUpForm>();
 
-      const onSubmit = (data: SignUpUser) => {
+      const onSubmit = (data: SignUpForm) => {
+            const [year, month, day] = data.birthday.split('-');
+            const birthday = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+            servicesUser.add({...data, birthday});
       }
+
       return (
             <Form className='form' onSubmit={handleSubmit(onSubmit)}>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Group className="mb-3" controlId="signupName">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" {...register('name')} />
+                        <Form.Control type="text" {...register('name')} autoComplete='on' />
                         <Form.Text className="text-danger d-none">
                               Credenciales incorrectas
                         </Form.Text>
                   </Form.Group>
 
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Group className="mb-3" controlId="signupLastname">
                         <Form.Label>Lastname</Form.Label>
-                        <Form.Control type="text" {...register('lastname')} />
+                        <Form.Control type="text" {...register('lastname')} autoComplete='on' />
                         <Form.Text className="text-danger d-none">
                               Credenciales incorrectas
                         </Form.Text>
                   </Form.Group>
 
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Group className="mb-3" controlId="signupBirthday">
                         <Form.Label>Birthday</Form.Label>
-                        <Form.Control type="date" {...register('birthdate')} />
+                        <Form.Control type="date" {...register('birthday')} autoComplete='on' />
                         <Form.Text className="text-danger d-none">
                               Credenciales incorrectas
                         </Form.Text>
                   </Form.Group>
 
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Group className="mb-3" controlId="signupEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" {...register('email')} />
+                        <Form.Control type="email" {...register('email')} autoComplete='on' />
                         <Form.Text className="text-danger d-none">
                               Credenciales incorrectas
                         </Form.Text>
                   </Form.Group>
 
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Group className="mb-3" controlId="signupPass">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" {...register('pass')} />
+                        <Form.Control type="password" {...register('pass')} autoComplete='on' />
                   </Form.Group>
 
                   <Button variant="primary" type="submit">
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,8 +4,26 @@ export type User = {
       email: string;
       lastname: string;
       pass: string;
-      birthdate: Date;
+      birthday: Date;
 }
 
-export type LoginUser = Omit<User, 'id' | 'lastName' | 'birthdate'>;
-export type SignUpUser = Omit<User, 'id'>;
+export type SignUpForm = Omit<User, 'id' | 'birthday'> & { birthday: string;}
+export type LoginUser = Omit<User, 'id' | 'lastname' | 'birthday'>;
+export type SignUpPayload = Omit<User, 'id'>;
+
+export type Movie = {
+      adult: boolean,
+      backdrop_path: string,
+      genre_ids: Number[],
+      id: Number,
+      original_language: string,
+      original_title: string,
+      overview: string,
+      popularity: Number,
+      poster_path: string,
+      release_date: string,
+      title: string,
+      video: boolean,
+      vote_average: Number
+      vote_count: Number
+}
